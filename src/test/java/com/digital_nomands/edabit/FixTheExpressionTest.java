d07effd04a7a34dc6a688e14a48a6615740a69c5
Aiperi <aibige24wka@gmail.com>
2024-12-25 21:08:49 +0600

    Is seven

---
+++ src/test/java/com/digital_nomands/edabit/FixTheExpressionTest.java
@@ -0,0 +1,19 @@
+package com.digital_nomands.edabit;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static com.digital_nomands.edabit.FixTheExpression.isSeven;
+
+public class FixTheExpressionTest {
+    @Test
+    public void isSevenShouldReturnTrue() {
+        Assert.assertTrue(isSeven(7));
+    }
+
+    @Test
+    public void isSevenShouldReturnFalse() {
+        Assert.assertFalse(isSeven(9));
+    }
+}
